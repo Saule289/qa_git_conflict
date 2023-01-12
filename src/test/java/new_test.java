41168fd8f46a7ac215d92731ae0d2ff7dd778d8f
Saule289 <saule289@mail.ru>
2023-01-12 08:46:42 +0400

    first test added

---
+++ src/test/java/new_test.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class new_test {
+
+    @Test
+    void newTest() {
+        Assertions.assertTrue(5 > 3);
+    }
+}
