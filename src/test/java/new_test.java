e1ca73710700260f54f8e1cf083ec8f825036368
Saule289 <saule289@mail.ru>
2023-01-12 21:51:00 +0400

    add my_test_ignore2

--- src/test/java/new_test.java
+++ src/test/java/new_test.java
@@ -2,11 +2,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class new_test {
-
     @Test
     void newTest() {
         Assertions.assertTrue(5 >= 2);
     }
+
 
     $("div").shouldhave("my_test_ignore2");
     System.Logger.Level("Conflict");
