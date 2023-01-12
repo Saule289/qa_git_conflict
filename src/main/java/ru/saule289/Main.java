d9ec5113bfd26feead18f54a11c9d65eb94dd6ba
Saule <77831706+Saule289@users.noreply.github.com>
2023-01-12 08:38:23 +0400

    Update Main.java
    add additional text in main

--- src/main/java/ru/saule289/Main.java
+++ src/main/java/ru/saule289/Main.java
@@ -2,6 +2,6 @@ package ru.saule289;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!!!!! Be Brave");
     }
 }
